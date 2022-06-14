70b1f03a87cdde8c0594bb0f532d163cd8ab4bed
Shantanu-14 <shantanubhil14@gmail.com>
2022-06-15 01:15:44 +0530

    url fixed

--- src/URLConfig.js
+++ src/URLConfig.js
@@ -1,3 +1,3 @@
 // export const BACKEND_SERVER = "https://textstore.herokuapp.com/api"
-// export const BACKEND_SERVER = "https://textstore.herokuapp.com/api"
-export const BACKEND_SERVER = "https://8e9d-124-123-182-89.in.ngrok.io/api"
+export const BACKEND_SERVER = "https://textstore.herokuapp.com/api"
+// export const BACKEND_SERVER = "https://8e9d-124-123-182-89.in.ngrok.io/api"
